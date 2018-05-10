96511e1abe6bdc9e17ae8ba317bf3b6564bd887b
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-05-10 04:04:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,5 +37,10 @@ def sum_array (num)
 end
 
 def add_s (array)
-  array.each_with_index.collect { |element, index| element[index] << "s"}
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+      element << "s"
+    end
 end

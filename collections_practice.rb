8e593601a102f661fd5072a4b2e1dcaab60f49f3
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-05-10 02:45:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+def sort_array_asc (ascending)
+  ascending.sort
+end
 
+def sort_array_desc (descending)
+  descending.sort.reverse
+end
+
+def sort_array_char_count (char_count)
+  char_count.sort_by {|x| x.length}
+end
+
+def swap_elements (elements)
+  elements[0], elements[1], elements[2] = elements[0], elements[2], elements[1]
+end
+
+def reverse_array (reverse)
+  reverse
+end
